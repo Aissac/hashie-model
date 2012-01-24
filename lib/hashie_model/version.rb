4eca016ed6568a952f78dd6388f71c5865866868
Florin Oltean <florin555@gmail.com>
2012-01-24 15:08:48 +0200

    Hashie is now Hashie2.

--- lib/hashie_model/version.rb
+++ lib/hashie_model/version.rb
@@ -1,8 +1,8 @@
 module HashieModel
   module Version
     MAJOR = 1
-    MINOR = 2
-    PATCH = 3
+    MINOR = 3
+    PATCH = 0
     BUILD = nil
     
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
