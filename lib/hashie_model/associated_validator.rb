0920f84d05da8729dde6f94c9e176984c79bfbd1
Florin Oltean <florin555@gmail.com>
2013-11-15 19:17:32 +0200

    Bugfix for validates_associated.
    validates_associated would not detect invalid objects in an association when one of the objects in the collection was invalid, but the first object was valid.

--- lib/hashie_model/associated_validator.rb
+++ lib/hashie_model/associated_validator.rb
@@ -1,9 +1,12 @@
 module HashieModel
   class AssociatedValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
-      return if (value.is_a?(Array) ? value : [value]).collect{ |r| r.nil? || r.valid? }.all?
-      errors = value.is_a?(Array) ? value.map(&:errors).first : value.errors
-      record.errors.add(attribute, errors, options.merge(:value => value))
+      collection = value.is_a?(Array) ? value : [value]
+      invalid_elements = collection.select { |e| e && !e.valid? }
+      if invalid_elements.present?
+        first_error = invalid_elements.first.errors
+        record.errors.add(attribute, first_error, options.merge(value: value))
+      end
     end
   end
 end
