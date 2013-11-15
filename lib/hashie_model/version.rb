7bf00fdc875c400640aae3680eb09420df1923d3
Istvan Hoka <istvan.hoka@gmail.com>
2013-11-15 19:44:54 +0200

    Bump version

--- lib/hashie_model/version.rb
+++ lib/hashie_model/version.rb
@@ -2,8 +2,8 @@ module HashieModel
   module Version
     MAJOR = 1
     MINOR = 3
-    PATCH = 8
-    BUILD = 60835584
+    PATCH = 9
+    BUILD = nil
     
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
